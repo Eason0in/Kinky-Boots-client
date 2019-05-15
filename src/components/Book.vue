dbae8fd7f53e23abd8e19de76a9e1864d8791bee
YA-CHEN <xiao6108@gmail.com>
2019-05-15 14:41:11 +0800

    Combine book

--- src/components/Book.vue
+++ src/components/Book.vue
@@ -1,41 +1,3 @@
-<<<<<<< HEAD
-<template lang="pug">
-  #app
-    form.wrapper
-      .form-group.red
-        input#name(type="text",required,v-model="book.name")
-        label(for="name",data-content="UserName") UserName
-      .form-group.red
-        input#telno(type="text",required,v-model="book.telno")
-        label(for="telno",data-content="TelNo") TelNo
-      .form-group.red(style='width:30%')
-        input#book-date(type="date",required,v-model="book.nowDate")
-        label(for="book-date",data-content="Date") Date
-      .form-group.red
-        input#ticket(type="number",required,v-model="book.ticket")
-        label(for="ticket",data-content="Ticket") Ticket
-      .form-group.red(style='width:30%')
-        button.tourBtn(type="submit") submit
-
-</template>
-
-<script>
-  export default {
-    name: 'Book',
-    data() {
-      return {
-        book: {
-          nowDate: '2019-05-15',
-          name: '',
-          ticket: null,
-          telno: ''
-        }
-      }
-    },
-    components: {
-      Datepicker
-    }
-=======
 <template>
   <div id="app">
     <div class="container">
@@ -59,6 +21,27 @@
           <br>
           <h4>Total £{{now_area.price*ticket}}</h4>
         </div>
+        <form class="wrapper">
+          <div class="form-group red">
+            <input id="name" type="text" required="required" v-model="book.name">
+            <label for="name" data-content="UserName">UserName</label>
+          </div>
+          <div class="form-group red">
+            <input id="telno" type="text" required="required" v-model="book.telno">
+            <label for="telno" data-content="TelNo">TelNo</label>
+          </div>
+          <div class="form-group red" style="width:30%;">
+            <input id="book-date" type="date" required="required" v-model="book.nowDate">
+            <label for="book-date" data-content="Date">Date</label>
+          </div>
+          <div class="form-group red">
+            <input id="ticket" type="number" required="required" v-model="book.ticket">
+            <label for="ticket" data-content="Ticket">Ticket</label>
+          </div>
+          <div class="form-group red" style="width:30%;">
+            <button class="tourBtn" type="submit">submit</button>
+          </div>
+        </form>
       </div>
       <div class="svg_area">
         <svg
@@ -131,8 +114,15 @@
 
 <script>
 export default {
+  name: "Book",
   data: function() {
     return {
+      book: {
+        nowDate: "2019-05-15",
+        name: "",
+        ticket: null,
+        telno: ""
+      },
       filter: "",
       ticket: 1,
       stage_data: [
@@ -176,134 +166,15 @@ export default {
       var tagname = $(this).attr("data-name");
       vm.filter = tagname;
     });
->>>>>>> 12c2e293b6ce9398a7fc89c12f6e1678475e247a
+  },
+  components: {
+    Datepicker
   }
 };
 </script>
 
 
 <style lang="sass" scoped>
-<<<<<<< HEAD
-  #app 
-    width: 50%
-
-  .tourBtn 
-    width: 100%
-    height: 40px
-    font-size: 18px
-    border-radius: 10px
-    margin-top: 20px
-    background-color: black
-    color: #f1f1f1
-    border: solid black
-    cursor: pointer
-    transition: 0.5s
-
-
-  .tourBtn:hover 
-    margin-top: 10px
-    background-color: black
-    color: #f1f1f1
-    border: solid black
-    box-shadow: 0px 5px 0px 0px rgba(0, 0, 0, 0.4)
-
-  .wrapper
-      align-items: center
-      display: flex
-      flex-direction: column
-      justify-content: center
-
-  $sans-serif: Roboto, Arial, sans-serif
-
-  .form-group
-      display: inline-block
-      height: 3rem
-      padding-top: 2rem
-
-      input
-          background: none
-          border: none
-          box-sizing: border-box
-          display: block
-          font-family: $sans-serif
-          font-size: 1rem
-          padding: 0.25rem 0.1rem
-          width: 100%
-
-      input:invalid
-          box-shadow: none
-
-      input:focus
-          outline: none
-
-      label
-          display: inline-block
-          font-family: $sans-serif
-          font-size: 0
-          pointer-events: none
-          position: relative
-          text-transform: uppercase
-          width: 100%
-
-      label::before
-          content: attr(data-content)
-          position: relative
-          transition: all 0.2s ease
-          will-change: font-size, top
-
-      label::after
-          bottom: 0.9rem
-          content: ''
-          height: 0.3rem
-          left: 50%
-          position: absolute
-          transition: all 0.2s ease
-          width: 0
-          will-change: width, left
-
-      label::before, input[required]:focus ~ label::before
-          font-size: 0.75rem
-          top: -3.25rem
-
-      input:focus ~ label::after
-          left: 0%
-          width: 100%
-
-      input:invalid ~ label::before
-          font-size: 1rem
-          top: -2rem
-
-  // ---------- CONSTRUCTOR
-  @mixin form-group($name, $lightcolor, $color, $darkcolor)
-      .form-group.#{$name}
-          input
-              border-bottom: 0.1rem solid $color
-              color: $darkcolor
-
-          label::after
-              background: $color
-
-          label::before, input[required]:focus ~ label::before
-              color: $lightcolor
-          
-          input:invalid ~ label::before
-              color: $color
-
-  // ---------- INCLUDE COMPONENTS
-  $blue: #03A9F4
-  $lightblue: lighten($blue, 15%)
-  $darkblue: darken($blue, 15%)
-
-  $red: #d32323
-  $lightred: lighten($red, 15%)
-  $darkred: darken($red, 15%)
-
-  @include form-group("blue", $lightblue, $blue, $darkblue)
-  @include form-group("red", $lightred, $red, $darkred)
-=======
-*
-  margin: 0
-  padding: 0
 .container
   margin-top: 8%
   display: grid
@@ -353,5 +224,120 @@ input
   border: solid 2px #d32323
   background-color: transparent
   cursor: pointer
->>>>>>> 12c2e293b6ce9398a7fc89c12f6e1678475e247a
+#app 
+  width: 50%
+
+.tourBtn 
+  width: 100%
+  height: 40px
+  font-size: 18px
+  border-radius: 10px
+  margin-top: 20px
+  background-color: black
+  color: #f1f1f1
+  border: solid black
+  cursor: pointer
+  transition: 0.5s
+
+
+.tourBtn:hover 
+  margin-top: 10px
+  background-color: black
+  color: #f1f1f1
+  border: solid black
+  box-shadow: 0px 5px 0px 0px rgba(0, 0, 0, 0.4)
+
+.wrapper
+    align-items: center
+    display: flex
+    flex-direction: column
+    justify-content: center
+
+$sans-serif: Roboto, Arial, sans-serif
+
+.form-group
+    display: inline-block
+    height: 3rem
+    padding-top: 2rem
+
+    input
+        background: none
+        border: none
+        box-sizing: border-box
+        display: block
+        font-family: $sans-serif
+        font-size: 1rem
+        padding: 0.25rem 0.1rem
+        width: 100%
+
+    input:invalid
+        box-shadow: none
+
+    input:focus
+        outline: none
+
+    label
+        display: inline-block
+        font-family: $sans-serif
+        font-size: 0
+        pointer-events: none
+        position: relative
+        text-transform: uppercase
+        width: 100%
+
+    label::before
+        content: attr(data-content)
+        position: relative
+        transition: all 0.2s ease
+        will-change: font-size, top
+
+    label::after
+        bottom: 0.9rem
+        content: ''
+        height: 0.3rem
+        left: 50%
+        position: absolute
+        transition: all 0.2s ease
+        width: 0
+        will-change: width, left
+
+    label::before, input[required]:focus ~ label::before
+        font-size: 0.75rem
+        top: -3.25rem
+
+    input:focus ~ label::after
+        left: 0%
+        width: 100%
+
+    input:invalid ~ label::before
+        font-size: 1rem
+        top: -2rem
+
+// ---------- CONSTRUCTOR
+@mixin form-group($name, $lightcolor, $color, $darkcolor)
+    .form-group.#{$name}
+        input
+            border-bottom: 0.1rem solid $color
+            color: $darkcolor
+
+        label::after
+            background: $color
+
+        label::before, input[required]:focus ~ label::before
+            color: $lightcolor
+        
+        input:invalid ~ label::before
+            color: $color
+
+// ---------- INCLUDE COMPONENTS
+$blue: #03A9F4
+$lightblue: lighten($blue, 15%)
+$darkblue: darken($blue, 15%)
+
+$red: #d32323
+$lightred: lighten($red, 15%)
+$darkred: darken($red, 15%)
+
+@include form-group("blue", $lightblue, $blue, $darkblue)
+@include form-group("red", $lightred, $red, $darkred)
 </style>
