c56f8879a5c7f0e945f34ac4e787065a41c3186a
YA-CHEN <xiao6108@gmail.com>
2019-05-16 12:48:29 +0800

    Change book page again

--- src/components/Book.vue
+++ src/components/Book.vue
@@ -8,36 +8,32 @@
           <p class="address">Her Majesty's Theatre, London</p>
         </div>
         <form class="wrapper">
-          <div class="form-group red" style="display: block">
+          <div class="form-group red">
             <input
               class="tag"
               style="width:30%;"
               id="tag"
               type="text"
               required="required"
-              v-model="filter"
+              v-model="areas[0]"
             >
             <label for="tag" data-content="選擇區域"></label>
-            <div class="ticket" v-if="now_area">
-              <h3>座位{{now_area.tag}}區 ｜ {{now_area.place}} ｜ 價格 £{{now_area.price}}</h3>
-            </div>
-          </div>
-
-          <div class="form-group red">
-            <input id="name" type="text" required="required" v-model="book.name">
-            <label for="name" data-content="UserName">UserName</label>
-          </div>
-          <div class="form-group red">
-            <input id="telno" type="text" required="required" v-model="book.telno">
-            <label for="telno" data-content="TelNo">TelNo</label>
           </div>
           <div class="form-group red">
             <input id="book-date" type="date" required="required" v-model="book.nowDate">
-            <label for="book-date" data-content="Date">Date</label>
+            <label for="book-date" data-content="觀看日期">觀看日期</label>
+          </div>
+          <div class="form-group red">
+            <input id="name" type="text" required="required" v-model="book.name">
+            <label for="name" data-content="購票姓名">購票姓名</label>
+          </div>
+          <div class="form-group red">
+            <input id="telno" type="text" required="required" v-model="book.telno">
+            <label for="telno" data-content="手機號碼">手機號碼</label>
           </div>
           <div class="form-group red">
             <input id="ticket" type="number" required="required" v-model="book.ticket">
-            <label for="ticket" data-content="Ticket">Ticket</label>
+            <label for="ticket" data-content="選擇票數">選擇票數</label>
           </div>
           <div class="form-group red" style="width:30%;">
             <button class="tourBtn" type="submit">submit</button>
@@ -60,6 +56,7 @@
           <g id="A1">
             <polygon
               data-name="A1"
+              @click="add('A1')"
               points="424.331,258.97 397.331,258.97 397.331,231.97 370.331,231.97 370.331,204.97 342.491,204.97 315.491,204.97 
         288.491,204.97 261.491,204.97 235.331,204.97 235.331,231.97 208.331,231.97 208.331,258.97 181.331,258.97 181.331,285.97 
         424.331,285.97 	"
@@ -68,12 +65,14 @@
           <g id="A2">
             <polygon
               data-name="A2"
+              @click="add('A2')"
               points="570.822,168.813 549.768,151.881 566.661,130.874 545.605,113.943 545.661,113.874 523.841,96.328 
         507.003,117.266 486.063,100.428 418.001,185.067 438.872,201.85 421.94,222.906 442.996,239.836 442.94,239.906 463.996,256.836 
         463.94,256.906 484.928,273.782 570.768,168.881 	"
             ></polygon>
             <polygon
               data-name="A2"
+              @click="add('A2')"
               points="34,168.813 55.055,151.881 38.161,130.874 59.217,113.943 59.161,113.874 80.981,96.328 97.819,117.266 
         118.759,100.428 186.821,185.067 165.95,201.85 182.882,222.906 161.826,239.836 161.882,239.906 140.826,256.836 140.882,256.906 
         119.895,273.782 34.055,168.881 	"
@@ -82,6 +81,7 @@
           <g id="B1">
             <polygon
               data-name="B1"
+              @click="add('B1')"
               points="423.911,302.97 180.911,302.97 154.331,302.97 154.331,329.97 181.331,329.97 181.331,356.97 208.331,356.97 
         208.331,383.97 234.331,383.97 234.331,410.97 261.331,410.97 288.331,410.97 315.331,410.97 342.331,410.97 369.331,410.97 
         369.331,383.97 369.491,383.97 397.331,383.97 397.331,356.97 424.331,356.97 424.331,329.97 451.331,329.97 451.331,302.97 	"
@@ -90,12 +90,14 @@
           <g id="B2">
             <polygon
               data-name="B2"
+              @click="add('B2')"
               points="604.822,195.813 583.768,178.881 497.928,283.782 480.94,304.906 501.928,321.782 485.556,342.14 485.487,342.085 
         467.94,363.906 489.761,381.452 506.692,360.397 506.761,360.452 540.624,318.342 540.761,318.452 557.692,297.397 
         557.761,297.452 592.146,254.693 571.09,237.761 	"
             ></polygon>
             <polygon
               data-name="B2"
+              @click="add('B2')"
               points="0,195.813 21.055,178.881 106.895,283.782 123.882,304.906 102.895,321.782 119.267,342.14 119.335,342.085 
         136.882,363.906 115.062,381.452 98.13,360.397 98.062,360.452 64.198,318.342 64.062,318.452 47.13,297.397 47.062,297.452 
         12.676,254.693 33.732,237.761 	"
@@ -126,6 +128,7 @@ export default {
       },
       filter: "",
       ticket: 1,
+      areas: [],
       stage_data: [
         {
           tag: "A1",
@@ -150,20 +153,11 @@ export default {
       ]
     };
   },
-  computed: {
-    now_area: function() {
-      var vobj = this;
-      var result = this.stage_data.filter(function(obj) {
-        return obj.tag == vobj.filter;
-      });
-      if (result.length == 0) {
-        return null;
-      }
-      return result[0];
-    }
-  },
   methods: {
-    filter() {}
+    add: function(data) {
+      this.areas.unshift(data);
+      console.log(this.areas);
+    }
   }
 };
 </script>
@@ -232,7 +226,6 @@ input
   border-radius: 10px
   background-color: black
   color: #f1f1f1
-  border: solid black
   cursor: pointer
   transition: 0.5s
 
@@ -254,7 +247,6 @@ $sans-serif: Roboto, Arial, sans-serif
     display: inline-block
     height: 3rem
     padding-top: 0.5rem
-    // border: solid 1px
 
     input
         background: none
@@ -262,7 +254,7 @@ $sans-serif: Roboto, Arial, sans-serif
         font-family: $sans-serif
         font-size: 1rem
         padding: 0.25rem 0.1rem
-        width: 90%
+        width: 50%
     input:invalid
         box-shadow: none
 
@@ -286,18 +278,17 @@ $sans-serif: Roboto, Arial, sans-serif
         will-change: font-size, top
 
     label::before, input[required]:focus ~ label::before
-        font-size: 0.75rem
-        top: -3.8rem
+        font-size: 0.7rem
+        top: -3.6rem
 
     input:invalid ~ label::before
-        font-size: 1rem
-        top: -2rem
+        font-size: 0.8rem
+        top: -2.1rem
 
 // ---------- CONSTRUCTOR
 @mixin form-group($name, $lightcolor, $color, $darkcolor)
     .form-group.#{$name}
         input
-            border-bottom: 0.1rem solid $color
             color: $darkcolor
 
         label::before, input[required]:focus ~ label::before
