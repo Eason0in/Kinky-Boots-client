0eefb1a2fcfcac04548b15e624976a2af21ccaab
YA-CHEN <xiao6108@gmail.com>
2019-05-17 21:38:11 +0800

    Add shop router

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from "vue";
 import Router from "vue-router";
 import Index from "@/components/Index";
 import Book from "@/components/Book";
+import Shop from "@/components/Shop";
 
 Vue.use(Router);
 
@@ -16,6 +17,11 @@ export default new Router({
       path: "/Book",
       name: "Book",
       component: Book
-    }
+    },
+    {
+      path: "/Shop",
+      name: "Shop",
+      component: Shop
+    },
   ]
 });
