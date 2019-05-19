25b26543454d531b0c1257d9be15066887236e2d
YA-CHEN <xiao6108@gmail.com>
2019-05-19 14:57:01 +0800

    Change by YACHEN

--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,8 @@
-<<<<<<< HEAD
-import Vue from 'vue'
-import Router from 'vue-router'
-import Index from '@/components/Index'
-import Book from '@/components/Book'
-=======
 import Vue from "vue";
 import Router from "vue-router";
 import Index from "@/components/Index";
 import Book from "@/components/Book";
 import Shop from "@/components/Shop";
-<<<<<<< HEAD
-=======
->>>>>>> 0eefb1a2fcfcac04548b15e624976a2af21ccaab
->>>>>>> 8d2e457e395eac24e9d47dbfb84f9661c0e1dda3
-
 Vue.use(Router)
 
 export default new Router({
